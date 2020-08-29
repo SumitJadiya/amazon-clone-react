c89c5c421bcffe6a695c60b44bbf57ca1a880a47
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-08-29 22:13:32 +0530

    minor bug on cart

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -7,26 +7,36 @@ import { getCartTotal } from '../context/reducer'
 function Subtotal() {
     const [{ cart }] = useStateValue()
     return (
-        <div className="subtotal">
-            <CurrencyFormat
-                renderText={value => (
-                    <>
-                        <p>
-                            Subtotal ({cart.length} items) : <strong>{`${value}`}</strong>
-                        </p>
-                        <small className="subtotal__gift">
-                            <input type="checkbox" /> This order contains a gift
+        <div class="subtotal">
+            <div class="subtotal__head">
+                <img
+                    src="https://images-eu.ssl-images-amazon.com/images/G/31/checkout/assets/TM_desktop._CB443006202_.png"
+                    alt=""
+                />
+            </div>
+
+            <div className="subtotal__body">
+                <CurrencyFormat
+                    renderText={value => (
+                        <>
+                            <p>
+                                Subtotal ({cart.length} items) : <strong>{`${value}`}</strong>
+                            </p>
+                            <small className="subtotal__gift">
+                                <input type="checkbox" /> This order contains a gift
                         </small>
-                    </>
-                )}
-                decimalScale={2}
-                value={getCartTotal(cart)}
-                displayType={"text"}
-                thousandSeparator={true}
-                prefix={"₹"}
-            />
-            <button>Proceed to Checkout</button>
+                        </>
+                    )}
+                    decimalScale={2}
+                    value={getCartTotal(cart)}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    prefix={"₹"}
+                />
+                <button>Proceed to Checkout</button>
+            </div>
         </div>
+
     )
 }
 

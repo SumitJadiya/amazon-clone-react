59773ba570cf0ca37670cbc7f0587b8a09f05b04
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-09-11 02:07:30 +0530

    introducing cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const express = require('express')
+const cors = require('cors')
+const stripe = require('stripe');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +10,24 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+// secret key from firebase
+
+
+
+// API
+
+// - App Config
+const app = express()
+
+// - Middleware
+app.use(cors({ origin: true }))
+app.use(express.json())
+
+// - API Routes
+app.get('/', (request, response) => response.status(200).send('hello world'))
+app.post('/payments/create', (request, response) => response.status(200).send('hello sumit'))
+
+// - Listen
+exports.api = functions.https.onRequest(app)

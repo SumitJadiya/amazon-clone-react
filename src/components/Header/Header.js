d18ee5e48c5b5141a15e40efce875722538c2b18
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-09-15 02:36:03 +0530

    fix secondary header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,92 +1,138 @@
-import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
-import SearchIcon from '@material-ui/icons/Search'
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import { useStateValue } from '../../context/StateProvider';
-import { auth } from '../../firebase/firebaseConfig';
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import RoomOutlinedIcon from "@material-ui/icons/RoomOutlined";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { useStateValue } from "../../context/StateProvider";
+import { auth } from "../../firebase/firebaseConfig";
 
 function Header() {
-    const [{ cart, user, profile }] = useStateValue()
+  const [{ cart, user, profile }] = useStateValue();
 
-    const login = () => {
-        if (user) {
-            auth.signOut()
-        }
+  const login = () => {
+    if (user) {
+      auth.signOut();
     }
+  };
 
-    return (
+  return (
+    <nav className="header">
+      <div className="header__top">
+        {/* Logo on left */}
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            alt=""
+          />
+        </Link>
 
-        <nav className="header">
-            {/* Logo on left */}
-            <Link to="/">
-                <img
-                    className="header__logo"
-                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                    alt="" />
+        {/* Search box */}
+        <div className="header__search">
+          <input type="text" className="header__searchInput" />
+          <SearchIcon className="header__searchIcon" />
+        </div>
+
+        <div className="header__flag"></div>
+
+        {/* 3 Links */}
+        <div className="header__nav">
+          {/* Link 1 - SignIn/SignOut */}
+          {user ? (
+            <Link to="/" className="header__link">
+              <div onClick={login} className="header__option">
+                <span className="header__optionLineOne">
+                  Hello, {user ? profile?.userName : "Guest"}
+                </span>
+                <span className="header__optionLineTwo">
+                  {user ? "Sign Out" : "Sign In"}
+                </span>
+              </div>
             </Link>
-
-            {/* Search box */}
-            <div className="header__search">
-                <input type="text" className="header__searchInput" />
-                <SearchIcon className="header__searchIcon" />
+          ) : (
+            <Link to="/login" className="header__link">
+              <div onClick={login} className="header__option">
+                <span className="header__optionLineOne">
+                  Hello, {user ? profile?.userName : "User"}
+                </span>
+                <span className="header__optionLineTwo">
+                  {user ? "Sign Out" : "Sign In"}
+                </span>
+              </div>
+            </Link>
+          )}
+          {/* Link 2 - Return Order */}
+          <Link to="/orders" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineOne">Return</span>
+              <span className="header__optionLineTwo">& Orders</span>
             </div>
-
-            <div className="header__flag">
+          </Link>
+          {/* Link 3 - Prime */}
+          <Link to="/" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineOne">Try</span>
+              <span className="header__optionLineTwo">Prime</span>
             </div>
-
-            {/* 3 Links */}
-            <div className="header__nav">
-                {/* Link 1 - SignIn/SignOut */}
-                {user ?
-                    <Link to="/" className="header__link">
-                        <div onClick={login} className="header__option">
-                            <span className="header__optionLineOne">Hello, {user ? profile?.userName : 'Guest'}</span>
-                            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
-                        </div>
-                    </Link>
-
-                    :
-                    <Link to="/login" className="header__link">
-                        <div onClick={login} className="header__option">
-                            <span className="header__optionLineOne">Hello, {user ? profile?.userName : 'User'}</span>
-                            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
-                        </div>
-                    </Link>
-                }
-                {/* Link 2 - Return Order */}
-                <Link to="/orders" className="header__link">
-                    <div className="header__option">
-                        <span className="header__optionLineOne">Return</span>
-                        <span className="header__optionLineTwo">& Orders</span>
-                    </div>
-                </Link>
-                {/* Link 3 - Prime */}
-                <Link to="/" className="header__link">
-                    <div className="header__option">
-                        <span className="header__optionLineOne">Your</span>
-                        <span className="header__optionLineTwo">Prime</span>
-                    </div>
-                </Link>
-                {/* Link 4 - Profile */}
-                <Link to="/profile" className="header__link">
-                    <div className="header__option">
-                        <span className="header__optionLineOne">Your</span>
-                        <span className="header__optionLineTwo">Profile</span>
-                    </div>
-                </Link>
-                {/* Basket*/}
-                <Link to="/cart" className="header__link">
-                    <div className="header__optionBasket">
-                        {/* Shopping Basket Icon*/}
-                        <ShoppingCartIcon />
-                        {/* Number of items in basket */}
-                        <span className="header__optionLineTwo basket__count">{cart?.length}</span>
-                    </div>
-                </Link>
+          </Link>
+          {/* Link 4 - Profile */}
+          <Link to="/profile" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineOne">Your</span>
+              <span className="header__optionLineTwo">Profile</span>
             </div>
-        </nav>
-    )
+          </Link>
+          {/* Basket*/}
+          <Link to="/cart" className="header__link">
+            <div className="header__optionBasket">
+              {/* Shopping Basket Icon*/}
+              <ShoppingCartIcon />
+              {/* Number of items in basket */}
+              <span className="header__optionLineTwo basket__count">
+                {cart?.length}
+              </span>
+            </div>
+          </Link>
+        </div>
+      </div>
+      <div className="header__bottom">
+        {/* Address */}
+        <div className="header__address">
+          <div className="header__address-icon">
+            <RoomOutlinedIcon />
+          </div>
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello</span>
+            <span className="header__optionLineTwo">Select your address</span>
+          </div>
+        </div>
+        {/* Nav */}
+        <div className="header__bottom-nav">
+          <span>Mobile</span>
+          <span>Best Sellers</span>
+          <span>Today's Deak</span>
+          <span>Prime</span>
+          <span>Computers</span>
+          <span>Pantry</span>
+          <span>Electronics</span>
+          <span>Books</span>
+          <span>New Release</span>
+          <span>Gift Ideas</span>
+          <span>Customer Service</span>
+          <span>Fashion</span>
+          <span>Amazon Pay</span>
+        </div>
+        {/* Advt */}
+        <div className="header__bottom-app">
+          <img
+            alt="Amazon App"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/MAI/Sweepstakes/June20/SWM_DownloadApp._CB410314483_.jpg"
+          />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
